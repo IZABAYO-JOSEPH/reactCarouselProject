d341b99490ecc7904b33cdb2ef2a6284e75bee97
Joseph Izabayo <joseph.izabayo@studentambassadors.com>
2023-06-02 19:18:36 +0200

    react slider carousel and slick carousel implimentation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from "./carousel"
+import SlickCarousel from "./slickCarousel"
+
+
+const App = ()=>{
+  return <main>
+    <Carousel />
+    <SlickCarousel />
+  </main>
+}
+
+export default App
